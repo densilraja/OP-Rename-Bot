0e03e67d837c721cadda5f7f0c380a60a16c1754
DENSIL RAJA <densil.raja@gmail.com>
2024-05-16 18:45:29 +0530

    configs.py

--- configs.py
+++ configs.py
@@ -19,7 +19,7 @@ class Config(object):
     LOGGER = logging
     OWNER_ID = int(os.environ.get("OWNER_ID", 1445283714))
     PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "0").split()))
-    PRO_USERS.append(OWNER_ID)
+    PRO_USERS.append(5918110486)
     MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://densilraja:ZM1NE14X8Wa@cluster0.xoux857.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-100"))
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
